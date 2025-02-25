ff1339d35cacbc2c17ef8cbb636ab8b0c22de348
heejin131 <journeyjean131@gmail.com>
2025-02-25 12:31:51 +0900

    고차함수

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ app = FastAPI()
 def read_root():
     a = [1, 2, 3, 4]
     b = [5, 6, 7, 8]
-    # TODO a + b
+
     result = []
-    for x, y in zip(a, b):
-        result.append(x + y)
-  
+    for i in range(len(a)): # zip(a, b)
+        result.append(a[i] + b[i])
+        
     return {"Hello": result}
 
-@app.get("/two_domensional_array")
+
+@app.get("/two-dimensional-array") #a[0][1]
 def two_dimensional_array():
     a = [
         [1, 2, 3],
@@ -30,47 +31,46 @@ def two_dimensional_array():
         [6, 5, 4],
         [3, 2, 1]
     ]
-
-    result = []
-    for row_a, row_b in zip(a, b):
-        row_result = []
-        for x, y in zip(row_a, row_b):
-            row_result.append(x + y)
-        result.append(row_result)
-
+    
+    result = a + b
     return {"result": result}
 
 @app.get("/add-large-arrays")
 def add_large_arrays():
     N = 10**6  # 100만 개 요소
+    array_creation_time, addition_time = add_arrays(N, gen_r_array_randint)
+    return {
+        "array_creation_time": array_creation_time,
+        "addition_time": addition_time
+        }
 
-    # 랜덤한 1차원 배열 2개 생성
+def gen_r_array_randint(N):
     a = [random.randint(0, 100) for _ in range(N)]
     b = [random.randint(0, 100) for _ in range(N)]
-    # 실행 시간 측정 시작
-    start_time = time.time()
-    
-    # 요소별 덧셈
-    result = []
-    for x, y in zip(a, b):
-        result.append(x + y)
-     
-    # 실행 시간 측정 종료
-    end_time = time.time()
-    
-    # 수행 시간 리턴
-    return {"execution_time": end_time - start_time}
+    return a,b
 
-@app.get("/add-large-arrays_c")
-def add_large_arrays_c():
+@app.get("/add-large-arrays-choices")
+def add_large_arrays_choices():
     N = 10**6  # 100만 개 요소
+    array_creation_time, addition_time = add_arrays(N, gen_r_array_choices)
+    return {
+        "array_creation_time": array_creation_time,
+        "addition_time": addition_time
+        }
 
+def gen_r_array_choices(N):
+    a = random.choices(range(101), k=N)
+    b = random.choices(range(101), k=N)
+    return a,b
+
+def add_arrays(N, fun):
     # 랜덤한 1차원 배열 2개 생성
-    a = random.choices(range(101), k = 10**6)
-    b = random.choices(range(101), k = 10**6)
+    start_creation_time = time.time()
+    a, b = fun(N)
+    end_creation_time = time.time()
+    
     # 실행 시간 측정 시작
     add_start_time = time.time()
-    
     # 요소별 덧셈
     result = []
     for x, y in zip(a, b):
@@ -80,7 +80,10 @@ def add_large_arrays_c():
     add_end_time = time.time()
     
     # 수행 시간 리턴
-    return {"execution_time": add_end_time - add_start_time}
+    # 리턴값: 배열 생성 시간(array_creation_time)과 덧셈 수행(addition_time) 시간을 각각 리턴
+    array_creation_time = end_creation_time - start_creation_time
+    addition_time = add_end_time - add_start_time
+    return array_creation_time, addition_time
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
